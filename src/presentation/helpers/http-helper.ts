54b18fb7f62406b6ba5d27da549993e0da079789
Afranio Dantas <afraniodantas0224@gmail.com>
2024-06-08 20:13:29 -0300

    refactor: change import

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
-import { ServerError } from '../errors/server-error';
 import { HttpResponse } from '../protocols/http';
+import { ServerError } from '../errors';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
