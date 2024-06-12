f690922ab7a178d24ddc27d2cbc27e0c43457b21
Afranio Dantas <afraniodantas0224@gmail.com>
2024-06-12 19:59:36 -0300

    chore: environment configuration in mongodb

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,31 @@
+import { MongoClient, Collection } from 'mongodb';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const MongoHelper = {
+  client: null as MongoClient,
+  uri: null as string,
+
+  async connect(uri: string): Promise<void> {
+    this.uri = uri;
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close();
+    this.client = null;
+  },
+
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri);
+    }
+    return this.client.db().collection(name);
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id });
+  },
+};
