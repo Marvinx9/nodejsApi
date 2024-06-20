92786baac2653658e95bd6b8878ea9f472d0b8a5
Afranio Dantas <afraniodantas0224@gmail.com>
2024-06-19 21:45:15 -0300

    test: ensure validation conposite returns the first error if more then one validation fails

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -6,35 +6,45 @@ import { ValidationComposite } from './validation-composite';
 
 interface SutTypes {
   sut: ValidationComposite;
-  validationStub: Validation;
+  validationStubs: Validation[];
 }
 
 const makeValidation = (): Validation => {
-  class validationStub implements Validation {
+  class validationStubs implements Validation {
     validate(input: any): Error {
       return null;
     }
   }
-  return new validationStub();
+  return new validationStubs();
 };
 
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidation();
-  const sut = new ValidationComposite([validationStub]);
+  const validationStubs = [makeValidation(), makeValidation()];
+  const sut = new ValidationComposite(validationStubs);
   return {
     sut,
-    validationStub,
+    validationStubs,
   };
 };
 
 describe('Validation Composite', () => {
   it('Should return an error if any validation fails', () => {
-    const { sut, validationStub } = makeSut();
+    const { sut, validationStubs } = makeSut();
     jest
-      .spyOn(validationStub, 'validate')
+      .spyOn(validationStubs[1], 'validate')
       .mockReturnValue(new MissingParamError('field'));
     const error = sut.validate({ field: 'any_value' });
     expect(error).toEqual(new MissingParamError('field'));
+  });
+
+  it('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut();
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValue(new Error());
+    jest
+      .spyOn(validationStubs[1], 'validate')
+      .mockReturnValue(new MissingParamError('field'));
+    const error = sut.validate({ field: 'any_value' });
+    expect(error).toEqual(new Error());
   });
 
   it('Should not return if validation succeeds', () => {
