f06e7f2ef5c98be7f4c1bd8597ed25b24efa92de
Afranio Dantas <afraniodantas0224@gmail.com>
2024-06-25 20:48:04 -0300

    refactor: move EmailValidatorAdapter to the main layer

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { makeSignupController } from '../factories/signup/signup';
-import { adaptRoute } from '../adapters/express-route-adapter';
+import { adaptRoute } from '../adapters/express/express-route-adapter';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
