8c571db95ea09661e20aff63da6b675cc8f0bcc7
Afranio Dantas <afraniodantas0224@gmail.com>
2024-06-25 21:52:04 -0300

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { makeSignupController } from '../factories/signup/signup';
 import { adaptRoute } from '../adapters/express/express-route-adapter';
+import { makeSignupController } from '../factories/signup/signup';
+import { makeLoginController } from '../factories/login/login-factory';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignupController()));
+  router.post('/login', adaptRoute(makeLoginController()));
 };
