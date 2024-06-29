3ef5d5a9ea65cd5a80e5b3222a167fcaf04be40f
Afranio Dantas <afraniodantas0224@gmail.com>
2024-06-29 19:42:07 -0300

    chore: config env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: 'mongodb://mongo:27017/clean-node-api',
+  mongoUrl: 'mongodb://mongo:27017/clean-node-api', //mongo
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || '%42%mavinx9',
 };
