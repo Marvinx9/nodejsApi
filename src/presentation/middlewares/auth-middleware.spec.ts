fcbdb47926ced4960a3219096e6d4c6a7461306f
Afranio Dantas <afranio.dantas@livsaude.com.br>
2024-07-05 12:22:57 -0300

    test: ensure create middleware in aplication

---
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,54 @@
+import { forbidden } from '../helpers/http/http-helper';
+import { AccessDeniedError } from '../errors';
+import { AuthMiddleware } from './auth-middleware';
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token';
+import { AccountModel } from '../../domain/models/account';
+import { Middleware } from '../protocols';
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@email.com',
+  password: 'hashed_password',
+});
+
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load(accessToken: string, role?: string): Promise<AccountModel> {
+      return new Promise((resolve) => resolve(makeFakeAccount()));
+    }
+  }
+  return new LoadAccountByTokenStub();
+};
+
+interface TypeSut {
+  sut: Middleware;
+  loadAccountByTokenStub: LoadAccountByToken;
+}
+
+const makeSut = (): TypeSut => {
+  const loadAccountByTokenStub = makeLoadAccountByToken();
+  const sut = new AuthMiddleware(loadAccountByTokenStub);
+  return { sut, loadAccountByTokenStub };
+};
+
+describe('Auth Middleware', () => {
+  it('Should return 403 if no x-access-token is found exists in headers', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  it('Should call LoadAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load');
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token',
+      },
+    });
+    expect(loadSpy).toHaveBeenCalledWith('any_token');
+  });
+});
