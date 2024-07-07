086e3484620d24b21d1fb2a34d63fec2af013a9c
Afranio Dantas <afraniodantas0224@gmail.com>
2024-07-07 11:05:27 -0300

    feat: integration Middleware on route post/survey

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -3,8 +3,11 @@ import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import { Collection } from 'mongodb';
 import { AddSurveyModel } from '../../domain/usecases/add-survey';
+import { sign } from 'jsonwebtoken';
+import env from '../config/env';
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 
 const makeSurveyData = (): AddSurveyModel => ({
   question: 'question',
@@ -31,12 +34,42 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await surveyCollection.deleteMany({});
   });
 
   describe('Post /surveys', () => {
-    it('Should return 204 on add survey success', async () => {
+    it('Should return 403 on add survey without accessToken', async () => {
       const surveyData = makeSurveyData();
-      await request(app).post('/api/surveys').send(surveyData).expect(204);
+      await request(app).post('/api/surveys').send(surveyData).expect(403);
+    });
+
+    it('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Afranio',
+        email: 'afraniodantas@gmail.com',
+        password: '123',
+        role: 'admin',
+      });
+      const id = res.ops[0]._id;
+      const accessToken = sign({ id }, env.jwtSecret);
+      await accountCollection.updateOne(
+        {
+          _id: id,
+        },
+        {
+          $set: {
+            accessToken,
+          },
+        },
+      );
+      const surveyData = makeSurveyData();
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send(surveyData)
+        .expect(204);
     });
   });
 });
