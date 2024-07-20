7a4fd13e557564a990e5bc9151f172c76d49c717
Afranio Dantas <afraniodantas0224@gmail.com>
2024-07-20 00:12:17 -0300

    chore: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import setupMiddlewares from './middlewares';
 import setupRouts from './routes';
+import setupSwagger from './config-swagger';
 
 const app = express();
+setupSwagger(app);
 setupMiddlewares(app);
 setupRouts(app);
 
