0614fb04f3c5ba3d383710e22bd1fa2a037a179c
Afranio Dantas <afraniodantas0224@gmail.com>
2024-07-21 15:26:30 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -7,7 +7,8 @@ export const surveyResultPath = {
     ],
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
-    description: 'Essa rota pode ser executada por **usuário logado**',
+    description:
+      'Essa rota só pode ser executada por **usuários authenticados**',
     parameters: [
       {
         in: 'path',
@@ -28,6 +29,49 @@ export const surveyResultPath = {
         },
       },
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'aplication/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/forbidden',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
+
+  get: {
+    security: [
+      {
+        apiKeyAuth: [],
+      },
+    ],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description:
+      'Essa rota só pode ser executada por **usuários authenticados**',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
     responses: {
       200: {
         description: 'Sucesso',
