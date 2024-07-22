4efe5251211beceae7b550aed1b004f8f25ba0c8
Afranio Dantas <afraniodantas0224@gmail.com>
2024-07-22 20:59:34 -0300

    feat: change load surveys to add didAnswer field

--- src/data/usecases/survey/load-surveys/db-load-surveys.ts
+++ src/data/usecases/survey/load-surveys/db-load-surveys.ts
@@ -7,8 +7,8 @@ import {
 export class DbLoadSurveys implements LoadSurveys {
   constructor(private readonly loadSurveysRepository: LoadSurveysRepository) {}
 
-  async load(): Promise<SurveyModel[] | null> {
-    const surveys = this.loadSurveysRepository.loadAll();
+  async load(accountId: string): Promise<SurveyModel[] | null> {
+    const surveys = this.loadSurveysRepository.loadAll(accountId);
     return surveys;
   }
 }
