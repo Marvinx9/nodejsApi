024e818c08d73d3a426f0a4c2922f86f1f61d108
Afrânio Nunes Dantas <afraniodantas0224@gmail.com>
2026-06-06 14:07:38 -0300

    feat: validation if group exists before add in databse

--- src/infra/db/mongodb/group/group-mongo-repository.ts
+++ src/infra/db/mongodb/group/group-mongo-repository.ts
@@ -1,11 +1,16 @@
 import { AddGroupRepository } from '../../../../data/protocols/db/group/add-group-repository';
+import { LoadGroupByIdRepository } from '../../../../data/protocols/db/group/load-group-by-id-repository';
 import { LoadGroupByNameRepository } from '../../../../data/protocols/db/group/load-group-by-name-repository';
 import { GroupModel } from '../../../../domain/models/group';
 import { AddGroupParams } from '../../../../domain/usecases/group/add-group';
 import { MongoHelper } from '../helpers';
+import { ObjectId } from 'mongodb';
 
 export class GroupMongoRepository
-  implements AddGroupRepository, LoadGroupByNameRepository
+  implements
+    AddGroupRepository,
+    LoadGroupByNameRepository,
+    LoadGroupByIdRepository
 {
   async add(groupData: AddGroupParams): Promise<GroupModel> {
     const groupCollection = await MongoHelper.getCollection('groups');
@@ -16,6 +21,12 @@ export class GroupMongoRepository
   async loadByName(name: string): Promise<GroupModel | null> {
     const groupCollection = await MongoHelper.getCollection('groups');
     const group = await groupCollection.findOne({ name });
+    return group && MongoHelper.map(group);
+  }
+
+  async loadById(id: string): Promise<GroupModel | null> {
+    const groupCollection = await MongoHelper.getCollection('groups');
+    const group = await groupCollection.findOne({ _id: new ObjectId(id) });
     return group && MongoHelper.map(group);
   }
 }
--- src/main/factories/usecases/survey/add-survey/db-add-survey-factory.ts
+++ src/main/factories/usecases/survey/add-survey/db-add-survey-factory.ts
@@ -1,8 +1,10 @@
 import { AddSurvey } from '../../../../../domain/usecases/survey/add-survey';
 import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository';
+import { GroupMongoRepository } from '../../../../../infra/db/mongodb/group/group-mongo-repository';
 import { DbAddSurvey } from '../../../../../data/usecases/survey/add-survey/db-add-survey';
 
 export const makeDbAddSurvey = (): AddSurvey => {
   const surveyMongoRepository = new SurveyMongoRepository();
-  return new DbAddSurvey(surveyMongoRepository);
+  const groupMongoRepository = new GroupMongoRepository();
+  return new DbAddSurvey(surveyMongoRepository, groupMongoRepository);
 };
