8f1dfb43672cf15b0669687e867219c17bc62aab
Afrânio Nunes Dantas <afraniodantas0224@gmail.com>
2026-06-06 13:58:03 -0300

    feat: add validation for create group required admin role

--- src/main/routes/group-routes.ts
+++ src/main/routes/group-routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapters/express-route-adapter';
 import { makeAddGroupController } from '../factories/controllers/group/add-group/add-group-controller-factory';
-import { auth } from '../middlewares/auth';
+import { adminAuth } from '../middlewares/admin-auth';
 
 export default (router: Router): void => {
-  router.post('/group', auth, adaptRoute(makeAddGroupController()));
+  router.post('/group', adminAuth, adaptRoute(makeAddGroupController()));
 };
