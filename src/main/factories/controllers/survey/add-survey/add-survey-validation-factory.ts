70cdaba068fe7de4524e79a37b363a925af0763f
Afrânio Nunes Dantas <afraniodantas0224@gmail.com>
2026-06-07 19:00:59 -0300

    feat: add validator for group id required

--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.ts
@@ -1,4 +1,5 @@
 import {
+  AtLeastOneCorrectAnswerValidation,
   ValidationComposite,
   RequiredFieldValidation,
 } from '../../../../../validation/validators';
@@ -6,8 +7,9 @@ import { Validation } from '../../../../../presentation/protocols/validation';
 
 export const makeAddSurveyValidation = (): ValidationComposite => {
   const validations: Validation[] = [];
-  for (const field of ['question', 'answers']) {
+  for (const field of ['question', 'groupId', 'answers']) {
     validations.push(new RequiredFieldValidation(field));
   }
+  validations.push(new AtLeastOneCorrectAnswerValidation('answers'));
   return new ValidationComposite(validations);
 };
