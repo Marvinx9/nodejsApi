f0e0e80a24281dacd2b9c82b60a62f105d4787f6
Afranio Nunes Dantas <afranio.dantas@livsaude.com.br>
2026-06-08 15:06:47 -0300

    fix: add correct typing in load survey by id

--- src/data/protocols/db/survey/load-survey-by-id-repository.ts
+++ src/data/protocols/db/survey/load-survey-by-id-repository.ts
@@ -2,8 +2,9 @@ import { SurveyModel } from '../../../../domain/models/survey';
 
 export interface LoadSurveyByIdRepository {
   loadById(id: string): Promise<
-    Omit<SurveyModel, 'answers'> & {
-      answers: Array<Omit<SurveyModel['answers'][0], 'isCorrectAnswer'>>;
-    }
+    | (Omit<SurveyModel, 'answers'> & {
+        answers: Array<Omit<SurveyModel['answers'][0], 'isCorrectAnswer'>>;
+      })
+    | null
   >;
 }
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,13 +1,16 @@
 import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository';
+import { LoadSurveyByIdRepository } from '../../../../data/protocols/db/survey/load-survey-by-id-repository';
 import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository';
 import { SurveyModel } from '../../../../domain/models/survey';
 import { AddSurveyParams } from '../../../../domain/usecases/survey/add-survey';
-import { LoadSurveyById } from '../../../../domain/usecases/survey/load-survey-by-id';
 import { MongoHelper, QueryBuilder } from '../helpers';
 import { ObjectId } from 'mongodb';
 
 export class SurveyMongoRepository
-  implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyById
+  implements
+    AddSurveyRepository,
+    LoadSurveysRepository,
+    LoadSurveyByIdRepository
 {
   async add(surveyData: AddSurveyParams): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys');
@@ -50,7 +53,12 @@ export class SurveyMongoRepository
     return MongoHelper.mapCollection(surveys);
   }
 
-  async loadById(id: string): Promise<SurveyModel | null> {
+  async loadById(id: string): Promise<
+    | (Omit<SurveyModel, 'answers'> & {
+        answers: Array<Omit<SurveyModel['answers'][0], 'isCorrectAnswer'>>;
+      })
+    | null
+  > {
     const surveyCollection = await MongoHelper.getCollection('surveys');
     const survey = await surveyCollection.findOne({ _id: new ObjectId(id) });
     return survey && MongoHelper.map(survey);
